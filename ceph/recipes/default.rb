f7f587222f74b7b58dd3c9b0fc37067aab4f506f
Maciej Galkiewicz <maciejgalkiewicz@ragnarson.com>
2011-10-03 12:56:46 +0200

    Added recipes for mds, mon, osd.

--- ceph/recipes/default.rb
+++ ceph/recipes/default.rb
@@ -7,19 +7,16 @@
 # All rights reserved - Do Not Redistribute
 #
 
-
 execute "add chef repo key" do
   command "wget -q -O- https://raw.github.com/NewDreamNetwork/ceph/master/keys/release.asc | apt-key add -"
   not_if { IO.popen("apt-key list").read.include?("1024D/288995C8") }
 end
 
-#distro = node[:lsb][:codename] if node[:platform] == "ubuntu"
-
 case node[:platform]
 when "ubuntu"
   distro = node[:lsb][:codename]
 when "debian"
-  distro = "squeeze"
+  distro = IO.popen("lsb_release -cs").read.chomp
 end
 
 template "/etc/apt/sources.list.d/ceph.list" do
@@ -38,9 +35,9 @@ package "ceph"
 template "/etc/ceph/ceph.conf" do
   source "ceph.conf.erb"
   variables(
-    :mon => data_bag_item("ceph", "mon"),
-    :mds => data_bag_item("ceph", "mds"),
-    :osd => data_bag_item("ceph", "osd")
+    :mon => search(:node, 'recipes:ceph\:\:mon'),
+    :mds => search(:node, 'recipes:ceph\:\:mds'),
+    :osd => search(:node, 'recipes:ceph\:\:osd')
   )
 end
 
